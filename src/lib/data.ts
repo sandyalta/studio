56ce4ad94d032c0c0d307d62eeac99edfefb0259
Sandy Tadete <sandytadete@gmail.com>
2025-10-29 11:24:16 +0000

    adding data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,51 +1,136 @@
 export const personalData = {
-  name: 'Jane Doe',
-  title: 'Full-Stack Developer',
-  bio: "A passionate Full-Stack Developer with experience in building web applications with JavaScript, React, Node.js, and Next.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems.",
+  name: 'Sandy Tadete',
+  title: 'Software Engineer II',
+  bio: "I plan to pursue my career as Programmer in one of the best top company in the world like Amazon, Google, etc. I need to improve my skills and knowledge in Computer Science area
+specially in Computer Vision in the best University outside
+Indonesia. I realized as a undergraduate student, I would have an
+opportunities to help my own country to move forward and
+develop in Technology specially in Artificial Intelligence.
+Detail-Oriented and Quick Learner with 5 Years Experiences on
+Application Engineer and 2 Years on Software Quality Engineer
+Software Engineer (Mobile/Web), with 5 year of experience in
+Software Engineer (Back End using Java and Microservices, Front
+End using React Js and React Native) also with 2 years experience
+in Software Quality Engineer field. My current role as Software
+Engineer, my deep understanding of frontend systems, including
+linting, deploying via pipelines, testing, and fast debugging, allows
+me to deliver effective solutions. Most importantly, I prioritize writing
+clean, easy-to-read code that ensures maintainability and clarity.
+Rumor has it that I excel in communication and collaboration
+within the team, as reflected in the positive feedback from my
+360-degree KPI assessments conducted by all team members.",
   contact: {
-    email: 'jane.doe@email.com',
-    tel: '+1 123 456 7890',
+    email: 'sandytadete@gmail.com',
+    tel: '+60 11 3967 6598 || +62 812 7626 7751',
     social: {
-      github: 'https://github.com',
-      linkedin: 'https://linkedin.com',
-      twitter: 'https://twitter.com',
+      github: 'https://github.com/sandyalta',
+      linkedin: 'https://www.linkedin.com/in/sandy-tadete/',
     },
   },
-  location: 'San Francisco, CA',
+  location: 'Kuala lumpur, Malaysia',
 };
 
 export const experienceData = [
   {
-    title: 'Senior Software Engineer',
-    company: 'Tech Solutions Inc.',
-    date: '2020 - Present',
+    title: 'Software Engineer II (React Native /React JS)',
+    company: 'Airasia MOVE',
+    date: 'Dec 2022 - Present',
     description:
-      'Led development of a new microservices-based platform, improving scalability and performance. Mentored junior developers and established best practices for code quality and testing.',
+      'Working on Ride e-hailing app with such as
+Driver app and Passenger app using React
+native project (typescript and react native,
+using Swift and Java for native). Business
+portal, Admin portal and Ride OTA (airport
+transfer) using React javascript project. Develop new features and support on current
+item within the team, working together with BE
+and QA team on every aspect of the projects.
+Do analysing code by write and also reviewing upon development process, creating unit
+testing using JEST for FE side and handling
+support DB using mongo-DB and Kubernetes
+Engine for server side.',
   },
   {
-    title: 'Software Engineer',
-    company: 'Innovate Co.',
-    date: '2018 - 2020',
+    title: 'React Native Developer (Part Time)',
+    company: 'Universe Action Limited',
+    date: 'May 2024 - Oct 2024',
     description:
-      'Developed and maintained features for a large-scale e-commerce website using React and Node.js. Collaborated with designers and product managers to deliver a seamless user experience.',
+      'Part-time job which built a Blockchain app
+using react native from scratch till released to
+production',
   },
   {
-    title: 'Junior Developer',
-    company: 'Web Wizards',
-    date: '2016 - 2018',
+    title: 'Senior Application Engineer (React Js and MS)',
+    company: 'Hitachi Ebworx Sdn Bhd',
+    date: 'Aug 2021 - Nov 2022',
     description:
-      'Assisted in building responsive websites for various clients. Gained foundational experience in HTML, CSS, JavaScript, and version control with Git.',
+      'Building web application using React Js and
+be a part as Leader for front end team on the
+project and also working for API side using MS
+(micro services). Debug log using OpenShift and using
+pgAdmin as databases. Working for OCBC bank Beyond Velocity
+project, handling front end team on deliver
+the project, using React Js in Micro-Frontend
+(MFE) frameworks.',
+  },
+  {
+    title: 'Senior Application Engineer (Mobile Developer)',
+    company: 'Hitachi Ebworx Indonesia',
+    date: 'June 2020 - July 2021',
+    description:
+      'Build mobile application using android studio
+(native android) and swift (native iOS). Do hybrid project using framework for both
+android and iOS (React native, Angular JS,
+etc) and do technical support by fixing bugs
+of the project. Working for RCM project on CIMB Bank,
+handle on the Android Java code.',
+  },
+  {
+    title: 'Mobile Application Developer (React Native)',
+    company: 'BIPO Service Indonesia',
+    date: 'May 2019 - March 2020',
+    description:
+      'Build application that truly native while using
+web technologies. Focusing in Front End of the product or
+application, understanding API services and
+infrastructure with which the app needs to integrate. Working for 2 project (Angular JS and React
+  Native), creating project React Native from
+  scratch.',
+  },
+  {
+    title: 'React Native Developer (Part Time)',
+    company: '9cv9 Tech Hiring and Offshoring',
+    date: 'Aug 2019 - Sep 2019',
+    description:
+      'Part-time remote as React Native Engineer.
+Focusing on front-end of the apps, build the
+application and do testing.',
+  },
+  {
+    title: 'Software Quality Engineer',
+    company: 'BIPO Service Indonesia',
+    date: 'Mar 2018 - Apr 2019',
+    description:
+      'Manual testing and bugs handling for both
+Web and Mobile application. Using Microsoft
+SQL Server as the tools to operate the
+databases. Reporting every testing case that divided by
+Trello. Maintenance bugs that reported by clients or
+Project Manager team and Increase
+productivity and reduce risk by raise new
+Trello. For 4 months long, our team have a project for
+Automation using Selenium and TestNG as the
+framework.',
   },
 ];
 
 export const projectsData = [
   {
     id: 'project-1',
-    title: 'E-commerce Platform',
+    title: 'HRMS System',
     description:
-      'A full-featured e-commerce platform with a modern UI, product management, and secure payments via Stripe.',
+      'A full revamping project HR Management System app which created from Angular JS App to React Native App from scratch (UI and API services)',
     image: '1',
-    tags: ['React', 'Next.js', 'Tailwind CSS', 'Stripe', 'Prisma'],
+    tags: ['Angular', 'React Native'],
     links: {
       github: '#',
       preview: '#',
@@ -53,36 +138,61 @@ export const projectsData = [
   },
   {
     id: 'project-2',
-    title: 'Project Management Tool',
+    title: 'RCS Mobile by CIMB Niaga',
     description:
-      'A collaborative tool for teams to manage tasks, track progress, and communicate effectively, with real-time updates.',
+      'Maintaining CIMB NIAGA Sales Mobile Apps for housing mortgage system',
     image: '2',
-    tags: ['Vue.js', 'Firebase', 'SCSS', 'Real-time DB'],
+    tags: ['Android Java'],
+    links: {
+      github: '#',
+      preview: 'https://play.google.com/store/apps/details?id=com.cimb.mortgage&hl=ms',
+    },
+  },
+  {
+    id: 'project-3',
+    title: 'OCBC bank Beyond Velocity Bill Payment',
+    description:
+      'Comprehensive internet banking platform for businesses that focusing on Bill Payment menu',
+    image: '3',
+    tags: ['React', 'MFE'],
+    links: {
+      github: '#',
+      preview: 'https://velocity.ocbc.com/login.html',
+    },
+  },
+  {
+    id: 'project-4',
+    title: 'PESO Token',
+    description:
+      'BlockChain App build from scratch',
+    image: '4',
+    tags: ['React Native'],
     links: {
       github: '#',
       preview: '#',
     },
   },
   {
-    id: 'project-3',
-    title: 'PortfolioFlow',
+    id: 'project-5',
+    title: 'Airasia Ride Driver & Ride',
     description:
-      'A personal portfolio website to showcase my work and skills, built with Next.js and featuring AI-powered CV data extraction.',
-    image: '3',
-    tags: ['Next.js', 'Genkit AI', 'Shadcn/ui', 'Tailwind'],
+      'airasia ride Driver is a mobile platform that connects drivers with passengers quickly and efficiently. Whether you are using your own car to earn extra income or driving full-time, our app helps you find nearby riders and optimise your time on the road.',
+    image: '5',
+    tags: ['React Native', 'React'],
     links: {
       github: '#',
-      preview: '#',
+      preview: 'https://play.google.com/store/apps/details?id=com.dacsee&hl=en',
     },
   },
 ];
 
 export const skillsData = [
   { name: 'JavaScript', level: 95 },
-  { name: 'TypeScript', level: 90 },
+  { name: 'TypeScript', level: 95 },
+  { name: 'React Native', level: 95 },
   { name: 'React / Next.js', level: 95 },
-  { name: 'Node.js', level: 85 },
+  { name: 'Node.js', level: 70 },
   { name: 'Databases (SQL/NoSQL)', level: 80 },
-  { name: 'Cloud (AWS/GCP)', level: 75 },
-  { name: 'UI/UX Design', level: 70 },
+  { name: 'Cloud (GCP)', level: 60 },
+  { name: 'UI/UX Design', level: 80 },
 ];
